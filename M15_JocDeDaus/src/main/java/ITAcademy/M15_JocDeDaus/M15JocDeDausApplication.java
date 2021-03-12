297a8c9e409cc2d286a7744163d0a5e4810ea7c9
Blamak <vlasherassanchez@gmail.com>
2021-03-12 11:18:30 +0100

    Project created

---
+++ M15_JocDeDaus/src/main/java/ITAcademy/M15_JocDeDaus/M15JocDeDausApplication.java
@@ -0,0 +1,13 @@
+package ITAcademy.M15_JocDeDaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M15JocDeDausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M15JocDeDausApplication.class, args);
+	}
+
+}
---
+++ M15_JocDeDaus/src/test/java/ITAcademy/M15_JocDeDaus/M15JocDeDausApplicationTests.java
@@ -0,0 +1,13 @@
+package ITAcademy.M15_JocDeDaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M15JocDeDausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
