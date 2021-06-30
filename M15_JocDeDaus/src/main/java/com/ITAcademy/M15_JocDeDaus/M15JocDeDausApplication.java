d4feed759ce5e7446a577149dea5b0613e568276
Blamak <vlasherassanchez@gmail.com>
2021-06-30 14:33:16 +0200

    final commit

--- M15_JocDeDaus/src/main/java/com/ITAcademy/M15_JocDeDaus/M15JocDeDausApplication.java
+++ M15_JocDeDaus/src/main/java/com/ITAcademy/M15_JocDeDaus/M15JocDeDausApplication.java
@@ -1,14 +1,7 @@
 package com.ITAcademy.M15_JocDeDaus;
 
-import java.sql.Timestamp;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.ITAcademy.M15_JocDeDaus.Entities.Player;
-import com.ITAcademy.M15_JocDeDaus.Repositories.IPlayerRepository;
 
 @SpringBootApplication
 public class M15JocDeDausApplication {
@@ -17,14 +10,4 @@ public class M15JocDeDausApplication {
 		SpringApplication.run(M15JocDeDausApplication.class, args);
 	}
 	
-//	@Bean
-//	public CommandLineRunner demo(IPlayerRepository playerRepository) {
-//		return (args) -> {
-//			// save players
-//			playerRepository.save(new Player(0, "SpongeBob", null, new Timestamp(System.currentTimeMillis())));
-//			playerRepository.save(new Player(0, "Patrick", null, new Timestamp(System.currentTimeMillis())));
-//			playerRepository.save(new Player(0, "SquidWard", null, new Timestamp(System.currentTimeMillis())));
-//		};
-//	}
-
 }
